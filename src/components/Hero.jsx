cbaa249597c17095554074a22c82579d83fce6cd
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-02 18:21:08 +0530

    hero section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import { FlipWords } from './ui/flip-words';
+import { heroImage } from '../constants';
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const words = ['thoughts', 'stories', 'ideas'];
 
-export default Hero
+  return (
+    <div className='h-screen flex items-center justify-center relative'>
+      <div className='w-full h-full flex flex-col items-center justify-center gap-10 overflow-hidden p-5'>
+        <div className=' text-black text-5xl font-semibold w-[700px]'>
+          Step into the world of Blogxpress, where our
+          <FlipWords words={words} />
+          come to life.
+        </div>
+        <div className='w-full flex items-center justify-center gap-10'>
+          {heroImage.map((curr) => (
+            <img
+              src={curr}
+              alt='hero'
+              className='w-[300px] h-[300px] rounded-3xl'
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+import hero1 from '../assets/hero1.jpeg';
+import hero2 from '../assets/hero2.jpeg';
+import hero3 from '../assets/hero3.jpeg';
+import hero4 from '../assets/hero4.jpeg';
+
+export const heroImage = [hero1, hero2, hero3, hero4];
