9982da1ace585bcdb7367a81e541d1ad170876de
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-02 16:23:24 +0530

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './sections/Home';
+import About from './sections/About';
+import Blog from './sections/Blog';
+import Footer from './components/Footer';
+import Error from './sections/Error';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blogs' element={<Blog />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
