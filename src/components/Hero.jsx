b080bb095d753cc7d85ef09810cbc52ee5f3b058
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-03 10:56:27 +0530

    Hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,28 @@ const Hero = () => {
   const words = ['thoughts', 'stories', 'ideas'];
 
   return (
-    <div className='h-screen flex items-center justify-center relative'>
-      <div className='w-full h-full flex flex-col items-center justify-center gap-10 overflow-hidden p-5'>
-        <div className=' text-black text-5xl font-semibold w-[700px]'>
-          Step into the world of Blogxpress, where our
-          <FlipWords words={words} />
-          come to life.
+    <div className='h-screen flex items-center justify-center relative overflow-hidden'>
+      <div className='w-[90%] h-full flex flex-col items-center justify-evenly    p-5'>
+        <div className='w-full flex items-start justify-start'>
+          <div className='  text-black text-5xl font-semibold w-[700px] '>
+            Step into the world of Blogxpress, where our
+            <FlipWords words={words} />
+            come to life.
+          </div>
         </div>
-        <div className='w-full flex items-center justify-center gap-10'>
-          {heroImage.map((curr) => (
-            <img
-              src={curr}
-              alt='hero'
-              className='w-[300px] h-[300px] rounded-3xl'
-            />
+        <div className='w-full flex items-center justify-evenly '>
+          {heroImage.map((curr, i) => (
+            <div
+              key={i}
+              className={`w-[400px] h-[400px] flex flex-col items-center ${
+                i % 2 !== 0 ? 'justify-start' : 'justify-center'
+              } `}>
+              <img
+                src={curr}
+                alt='hero'
+                className='w-[300px] h-[300px] rounded-3xl'
+              />
+            </div>
           ))}
         </div>
       </div>
