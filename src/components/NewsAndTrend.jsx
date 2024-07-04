b4eafa87a997b2f6cbb5fd34ecaeb1d561403eb6
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-04 23:11:56 +0530

    added  News&Trend section

--- src/components/NewsAndTrend.jsx
+++ src/components/NewsAndTrend.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
-
+import React from "react";
+import { newAndTrend } from "../constants";
+import { TbArrowWaveRightDown } from "react-icons/tb";
 const NewsAndTrend = () => {
-  return <div>NewsAndTrend</div>;
+  return (
+    <div className=" h-full flex items-center justify-center relative">
+      <div className="w-[90%] h-full flex flex-col items-center justify-center p-4">
+        <div className="w-full flex flex-col items-start justify-start ">
+          <h3 className=" text-[#FEAD6E] font-semibold text-xl">
+            NEWS & TRENDS
+          </h3>
+          <h2 className="text-3xl font-semibold">Game Changers Unleashed:</h2>
+          <h2 className="text-3xl font-semibold">
+            The Ultimate Sports Prodcast
+          </h2>
+        </div>
+        <div className="w-full flex items-center justify-evenly mt-5">
+          {newAndTrend.map((curr, i) => (
+            <div className="flex flex-col items-center justify-start w-full h-[600px]">
+              <div className=" relative flex flex-col items-start justify-start">
+                <img
+                  src={curr.image}
+                  alt="image"
+                  className=" w-[300px] h-[400px] rounded-3xl rounded-br-[11rem]    "
+                />
+                <div className="p-2 flex items-center justify-center rounded-tl-[2rem] absolute right-0 bg-white bottom-0">
+                  <button
+                    style={{
+                      backgroundColor: curr.color,
+                    }}
+                    className={`text-5xl p-2 text-white rounded-full delay-300 transition-transform duration-300  `}
+                  >
+                    <TbArrowWaveRightDown className="rotate-12 hover:rotate-45 delay-300 transition-transform duration-300" />
+                  </button>
+                </div>
+              </div>
+              <div className="w-[300px] flex flex-col items-left justify-between h-full ">
+                <h1
+                  className=" font-bold underline text-2xl"
+                  style={{
+                    textDecorationColor: curr.color,
+                    textDecorationThickness: "10px",
+                  }}
+                >
+                  {curr.title}
+                </h1>
+                <p className=" text-gray-600 text-lg">{curr.description}</p>
+                <div className=" flex items-center justify-left gap-5 text-white ">
+                  <button
+                    style={{ backgroundColor: curr.color }}
+                    className=" rounded-md p-1"
+                  >
+                    {curr.link1}
+                  </button>
+                  <button
+                    style={{ backgroundColor: curr.color }}
+                    className="rounded-md p-1"
+                  >
+                    {curr.link2}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsAndTrend;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette');
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme('colors'));
+  let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
   addBase({
-    ':root': newVars,
+    ":root": newVars,
   });
 }
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
         scroll:
-          'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       boxShadow: {
         custom:
-          '0px 10px 15px -3px rgba(0,0,0,0.1), 0px 10px 15px -3px rgba(0,0,0,0.1)',
+          "0px 10px 15px -3px rgba(0,0,0,0.1), 0px 10px 15px -3px rgba(0,0,0,0.1)",
       },
       keyframes: {
         scroll: {
           to: {
-            transform: 'translate(calc(-50% - 0.5rem))',
+            transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
