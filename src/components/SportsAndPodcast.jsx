4afff4bbc760dbbadb8b910796729c7f1de6c337
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-04 12:46:42 +0530

    SPORTS & PODCAST section complete

--- src/components/SportsAndPodcast.jsx
+++ src/components/SportsAndPodcast.jsx
@@ -3,46 +3,8 @@ import { MdArrowRightAlt } from 'react-icons/md';
 import { StickyScroll } from '../components/ui/sticky-scroll-reveal';
 import { content } from '../constants/index.jsx';
 import { InfiniteMovingCards } from './ui/infinite-moving-cards.tsx';
-import sports1 from '../assets/sports1.jpg';
-import sports2 from '../assets/sports2.jpg';
-import sports3 from '../assets/sports3.jpg';
-
-const testimonials = [
-  {
-    quote:
-      'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.',
-    name: 'Charles Dickens',
-    title: 'A Tale of Two Cities',
-    image: sports1,
-  },
-  {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: 'William Shakespeare',
-    title: 'Hamlet',
-    image: sports2,
-  },
-  {
-    quote: 'All that we see or seem is but a dream within a dream.',
-    name: 'Edgar Allan Poe',
-    title: 'A Dream Within a Dream',
-    image: sports3,
-  },
-  {
-    quote:
-      'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.',
-    name: 'Jane Austen',
-    title: 'Pride and Prejudice',
-    image: sports1,
-  },
-  {
-    quote:
-      'Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.',
-    name: 'Herman Melville',
-    title: 'Moby-Dick',
-    image: sports2,
-  },
-];
+import { explorMore } from '../constants/index.jsx';
+import { explorMor2 } from '../constants/index.jsx';
 
 const SportsAndPodcast = () => {
   return (
@@ -61,14 +23,28 @@ const SportsAndPodcast = () => {
           <StickyScroll content={content} />
         </div>
         <div className='h-[40rem] w-full rounded-md flex flex-col antialiased   items-center justify-center relative overflow-hidden text-start p-4'>
-          <div className='w-3/4'>
-            <h1 className=' font-bold text-3xl'>Explore More</h1>
+          <div className='w-3/4 flex items-center'>
+            <h1 className=' font-bold text-3xl '>Explore More</h1>
+            <MdArrowRightAlt className='text-3xl' />
           </div>
           <InfiniteMovingCards
-            items={testimonials}
+            items={explorMore}
             direction='right'
             speed='slow'
           />
+        </div>
+        <div className='w-full h-[400px] flex items-center justify-center gap-10'>
+          {explorMor2.map((curr, i) => (
+            <div
+              className={` ${
+                i === 1 ? 'bg-black rounded-3xl ' : 'bg-[#A192f3]'
+              } ${
+                i === 0 ? 'rounded-r-3xl ' : 'rounded-l-3xl'
+              }  h-full p-4 text-white flex flex-col items-left text-wrap justify-center`}>
+              <h1 className='text-3xl font-semibold'>{curr.title}</h1>
+              <p className='text-md'>{curr.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -107,7 +107,57 @@ export const content = [
   },
 ];
 
+export const explorMore = [
+  {
+    quote:
+      'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.',
+    name: 'Charles Dickens',
+    title: 'A Tale of Two Cities',
+    image: sports1,
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: 'William Shakespeare',
+    title: 'Hamlet',
+    image: sports2,
+  },
+  {
+    quote: 'All that we see or seem is but a dream within a dream.',
+    name: 'Edgar Allan Poe',
+    title: 'A Dream Within a Dream',
+    image: sports3,
+  },
+  {
+    quote:
+      'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.',
+    name: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    image: sports1,
+  },
+  {
+    quote:
+      'Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.',
+    name: 'Herman Melville',
+    title: 'Moby-Dick',
+    image: sports2,
+  },
+];
 
-export const explore = [
-  
-]
+export const explorMor2 = [
+  {
+    title: 'Possibility To Watch',
+    description:
+      'Participating in sports is instrumental in advancing physical health, nurturing collaboration, and instilling crucial virtues such as discipline, resilience, and a spirit of sportsmanship.',
+  },
+  {
+    title: 'Central Text',
+    description:
+      'Sports play a significant role in promoting physical fitness, fostering teamwork, and instilling values such as discipline, perseverance.',
+  },
+  {
+    title: 'Ability To Share With Friends',
+    description:
+      'Engaging in sports plays a pivotal role in enhancing physical well being, nurturing teamwork, and cultivating essential values like discipline, persistence, and sportsmanship.',
+  },
+];
