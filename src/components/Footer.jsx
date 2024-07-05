f4839b86f37ba43ee12d96c1f6126e4d5013d0b8
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-05 14:07:30 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='w-full flex items-start justify-evenly p-4 text-gray-500  mt-10'>
+      <div className='w-[30%] flex flex-col items-start'>
+        <h1 className='text-black text-3xl font-bold'>Blogxpress</h1>
+        <p>
+          Express insight, industry trends, and inspiring stories that help you
+          live and work on your own terms. Expert insights, industry trends.
+        </p>
+      </div>
+      <div className='flex flex-col items-start'>
+        <h1 className='text-black text-xl font-bold'>Home</h1>
+        <h3>Company</h3>
+        <h3>Quality Guarantee</h3>
+        <h3>Store Locations</h3>
+        <h3>Sustainability</h3>
+      </div>
+      <div className='flex flex-col items-start'>
+        <h1 className='text-black text-xl font-bold'>PAGE</h1>
+        <h3>Foods & Trends</h3>
+        <h3>Top Creator</h3>
+        <h3>News & Trends</h3>
+        <h3>Sports & Podcast</h3>
+      </div>
+      <div className='flex flex-col items-start'>
+        <h1 className='text-black text-xl font-bold'>Contact</h1>
+        <h3>+123456789</h3>
+        <h3>blogxpress@gmail.com</h3>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
