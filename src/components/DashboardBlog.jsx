8acea28e0d7ba7a70805901852ec6f7182f48776
Sh-karan27 <shuklakaran107@gmail.com>
2024-07-15 21:03:33 +0530

    cc

--- src/components/DashboardBlog.jsx
+++ src/components/DashboardBlog.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { userBlog } from '../store/slices/blogSlice';
-import { FaBookReader } from 'react-icons/fa';
-import { BsEyeFill } from 'react-icons/bs';
-import { RiEyeOffFill } from 'react-icons/ri';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userBlog } from "../store/slices/blogSlice";
+import { FaBookReader } from "react-icons/fa";
+import { BsEyeFill } from "react-icons/bs";
+import { RiEyeOffFill } from "react-icons/ri";
+import { toggleBlogPublished } from "../store/slices/blogSlice";
 
 const DashboardBlog = ({ id }) => {
   const dispatch = useDispatch();
@@ -23,29 +24,38 @@ const DashboardBlog = ({ id }) => {
     return <div>{error}</div>;
   }
 
+
+  const handleToggleBlogStatus = (id) => {
+    dispatch(toggleBlogPublished({ blogId: id }));
+  };
   console.log(data);
 
   return (
-    <div className='w-full flex flex-wrap justify-center'>
-      {data?.data.map((curr, i) => (
+    <div className="w-full flex flex-wrap justify-center">
+      {data?.map((curr, i) => (
         <div
           key={i}
-          className='flex flex-col items-left justify-left gap-1 my-4 mx-2 w-[20rem] h-full border rounded-lg overflow-hidden'>
-          <img src={curr.coverImage.url} alt='' className='w-full h-1/2' />
-          <div className='w-full flex flex-col items-left justify-left p-2'>
-            <h1 className='text-xl font-semibold'>{curr.title}</h1>
+          className="flex flex-col items-left justify-left gap-1 my-4 mx-2 w-[20rem] h-full border rounded-lg overflow-hidden"
+        >
+          <img src={curr.coverImage.url} alt="" className="w-full h-1/2" />
+          <div className="w-full flex flex-col items-left justify-left p-2">
+            <h1 className="text-xl font-semibold">{curr.title}</h1>
             <p>{curr.description}</p>
           </div>
-          <div className='w-full flex items-center justify-between p-2'>
-            <h1 className='text-xl semibold flex items-center justify-center gap-1 text-gray-500'>
+          <div className="w-full flex items-center justify-between p-2">
+            <h1 className="text-xl semibold flex items-center justify-center gap-1 text-gray-500">
               <FaBookReader />
-              <span className='text-gray-500'>{curr.views}</span>
+              <span className="text-gray-500">{curr.views}</span>
             </h1>
-            <h1 className='text-xl semibold flex items-center justify-center gap-1'>
+            <h1 className="text-xl semibold flex items-center justify-center gap-1">
               {curr.published === true ? (
-                <BsEyeFill className=' text-blue-500' />
+                <button onClick={handleToggleBlogStatus(curr._id)}>
+                  <BsEyeFill className=" text-blue-500" />
+                </button>
               ) : (
-                <RiEyeOffFill className='text-red-500' />
+                <button onClick={handleToggleBlogStatus(curr._id)}>
+                  <RiEyeOffFill className=" text-red-500" />
+                </button>
               )}
             </h1>
           </div>
--- src/store/slices/blogSlice.js
+++ src/store/slices/blogSlice.js
@@ -1,6 +1,6 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axiosInstance from '../../axiosInstance';
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../../axiosInstance";
+import { MdNature } from "react-icons/md";
 
 const initialState = {
   loading: null,
@@ -9,11 +9,26 @@ const initialState = {
 };
 
 export const userBlog = createAsyncThunk(
-  '/blog/fetchUserBlog',
+  "/blog/fetchUserBlog",
   async ({ userId }, { rejectWithValue }) => {
     try {
       const response = await axiosInstance.get(`/blog/u/${userId}`);
       console.log(response.data.data);
+      return response.data.data;
+    } catch (error) {
+      rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const toggleBlogPublished = createAsyncThunk(
+  "/blog/toogleBlogStatus",
+  async ({ blogId }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.patch(
+        `/blog/toggle/status/${blogId}`
+      );
+      console.log(response.data);
       return response.data;
     } catch (error) {
       rejectWithValue(error.response.data);
@@ -22,11 +37,12 @@ export const userBlog = createAsyncThunk(
 );
 
 const blogSlice = createSlice({
-  name: 'blog',
+  name: "blog",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
+
       .addCase(userBlog.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -38,6 +54,19 @@ const blogSlice = createSlice({
       .addCase(userBlog.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(toggleBlogPublished.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(toggleBlogPublished.rejected, (state, action) => {
+        state.loading = null;
+        state.error = action.payload;
+      })
+      .addCase(toggleBlogPublished.fulfilled, (state, action) => {
+        state.loading = null;
+        state.error = null;
+        state.data = action.payload;
       });
   },
 });
