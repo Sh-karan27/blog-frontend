75c5798fc5feb5d6ac3f477e60a2fff59fee1b12
Sh-karan27 <shuklakaran107@gmail.com>
2024-08-27 21:45:13 +0530

    cc

--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -1,91 +1,140 @@
-import React from 'react';
-import { CiImageOn } from 'react-icons/ci';
-import { FaChalkboardUser } from 'react-icons/fa6';
+import React, { useState } from "react";
+import { CiImageOn } from "react-icons/ci";
+import { FaChalkboardUser } from "react-icons/fa6";
 
-const Singup = ({ setSignUp }) => {
+const Signup = ({ setSignUp }) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    bio: "",
+    password: "",
+    coverImage: "",
+    profileImage: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: files ? files[0] : value, // Handle file inputs separately
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setFormData({
+      username: "",
+      email: "",
+      bio: "",
+      password: "",
+      coverImage: "",
+      profileImage: "",
+    });
+  };
+
   return (
-    <div className='w-[35rem] h-[50rem] glass-background rounded-lg shadow-lg flex flex-col items-center justify-center'>
-      <div className='w-full flex items-center justify-between p-8'>
-        <h1 className='text-3xl font-bold'>BlogExpress</h1>
+    <div className="w-[35rem] h-[50rem] glass-background rounded-lg shadow-lg flex flex-col items-center justify-center">
+      <div className="w-full flex items-center justify-between p-8">
+        <h1 className="text-3xl font-bold">BlogExpress</h1>
         <div
-          className='text-sm text-blue-400 flex flex-col items-end justify-end cursor-pointer'
-          onClick={() => setSignUp(false)}>
+          className="text-sm text-blue-400 flex flex-col items-end justify-end cursor-pointer"
+          onClick={() => setSignUp(false)}
+        >
           <span>Have an account?</span> <span>Sign in</span>
         </div>
       </div>
-      <div className='px-8 w-full'>
-        <h1 className='text-5xl font-bold'>Sign Up</h1>
+      <div className="px-8 w-full">
+        <h1 className="text-5xl font-bold">Sign Up</h1>
       </div>
-      <div className='w-full p-8'>
-        <form className='flex flex-col space-y-4'>
-          <label className='flex flex-col text-gray-400'>
+      <div className="w-full p-8">
+        <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+          <label className="flex flex-col text-gray-400">
             Username
             <input
-              type='text'
-              placeholder='Username'
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
               required
-              className='mt-1 p-2 rounded-md text-black focus:border-blue-500 border outline-none'
+              className="mt-1 p-2 rounded-md text-black focus:border-blue-500 border outline-none"
             />
           </label>
-          <label className='flex flex-col text-gray-400'>
+          <label className="flex flex-col text-gray-400">
             Email Address
             <input
-              type='email'
-              placeholder='Email'
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               required
-              className='mt-1 p-2 rounded-md text-black focus:border-blue-500 border outline-none'
+              className="mt-1 p-2 rounded-md text-black focus:border-blue-500 border outline-none"
             />
           </label>
-          <label className='flex flex-col text-gray-400'>
+          <label className="flex flex-col text-gray-400">
             Bio
             <input
-              type='text'
-              placeholder='Bio'
+              type="text"
+              placeholder="Bio"
+              name="bio"
+              value={formData.bio}
+              onChange={handleChange}
               required
-              className='mt-1 p-2 border outline-none text-black rounded-md focus:border-blue-500'
+              className="mt-1 p-2 border outline-none text-black rounded-md focus:border-blue-500"
             />
           </label>
-          <label className='flex flex-col text-gray-400'>
+          <label className="flex flex-col text-gray-400">
             Password
             <input
-              type='password'
-              placeholder='Password'
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
               required
-              className='mt-1 p-2 border outline-none text-black rounded-md focus:border-blue-500'
+              className="mt-1 p-2 border outline-none text-black rounded-md focus:border-blue-500"
             />
           </label>
 
           {/* Cover Image Input */}
-          <label className='flex items-center space-x-2 text-gray-400'>
+          <label className="flex items-center space-x-2 text-gray-400">
             <CiImageOn size={24} />
             <input
-              type='file'
-              accept='image/*'
-              className='hidden'
-              id='coverImageInput'
+              type="file"
+              name="coverImage"
+              accept="image/*"
+              className="hidden"
+              id="coverImageInput"
+              onChange={handleChange}
             />
-            <label htmlFor='coverImageInput' className='cursor-pointer'>
+            <label htmlFor="coverImageInput" className="cursor-pointer">
               Upload Cover Image
             </label>
           </label>
 
           {/* Profile Image Input */}
-          <label className='flex items-center space-x-2 text-gray-400'>
+          <label className="flex items-center space-x-2 text-gray-400">
             <FaChalkboardUser size={24} />
             <input
-              type='file'
-              accept='image/*'
-              className='hidden'
-              id='profileImageInput'
+              type="file"
+              accept="image/*"
+              className="hidden"
+              id="profileImageInput"
+              name="profileImage"
+              onChange={handleChange}
             />
-            <label htmlFor='profileImageInput' className='cursor-pointer'>
+            <label htmlFor="profileImageInput" className="cursor-pointer">
               Upload Profile Image
             </label>
           </label>
 
           <button
-            className='border-black px-4 py-2 w-1/4 rounded-md bg-black text-white'
-            type='submit'>
+            className="border-black px-4 py-2 w-1/4 rounded-md bg-black text-white"
+            type="submit"
+          >
             Sign up
           </button>
         </form>
@@ -94,4 +143,4 @@ const Singup = ({ setSignUp }) => {
   );
 };
 
-export default Singup;
+export default Signup;
