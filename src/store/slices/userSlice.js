df2b2c5d59e78d13bdf52e15e703af1d8a445681
Sh-karan27 <shuklakaran107@gmail.com>
2024-08-29 17:23:09 +0530

    cc

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -16,25 +16,21 @@ export const registerUser = createAsyncThunk(
   "auth/registerUser",
   async ({ formData }, { rejectWithValue }) => {
     try {
-      const multipartFormData = new FormData();
-      multipartFormData.append("username", formData.username);
-      multipartFormData.append("email", formData.email);
-      multipartFormData.append("password", formData.password);
-      multipartFormData.append("bio", formData.bio);
-      if (formData.coverImage)
-        multipartFormData.append("coverImage", formData.coverImage);
-      if (formData.profileImage)
-        multipartFormData.append("profileImage", formData.profileImage);
+      // const multipartFormData = new FormData();
+      // multipartFormData.append("username", formData.username);
+      // multipartFormData.append("email", formData.email);
+      // multipartFormData.append("password", formData.password);
+      // multipartFormData.append("bio", formData.bio);
+      // if (formData.coverImage)
+      //   multipartFormData.append("coverImage", formData.coverImage);
+      // if (formData.profileImage)
+      //   multipartFormData.append("profileImage", formData.profileImage);
 
-      const response = await axiosInstance.post(
-        "/users/register",
-        multipartFormData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      const response = await axiosInstance.post("/users/register", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
       const { accessToken, user } = response.data.data;
       localStorage.setItem("accessToken", accessToken);
