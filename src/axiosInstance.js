0283665f0e5dacf1145481475756fdce80a2faa6
Sh-karan27 <shuklakaran107@gmail.com>
2025-05-18 07:50:30 +0530

    Refactor axios instance configuration and clean up commented code

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,24 +1,33 @@
-// src/axiosInstance.js
+// // src/axiosInstance.js
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//   // baseURL: 'https://blog-api-ten-rust.vercel.app/api/v1',
+//   baseURL: 'http://localhost:8000/api/v1/',
+
+//   withCredentials: true, // Send cookies with requests
+// });
+
+// // Set default headers for all requests
+// axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+
+// // Optional: Add a request interceptor to include tokens
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem('accessToken'); // Retrieve your token from storage
+//     if (accessToken) {
+//       config.headers['Authorization'] = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: 'https://blog-api-ten-rust.vercel.app/api/v1',
-  withCredentials: true, // Send cookies with requests
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api/v1',
+  withCredentials: true,
 });
-
-// Set default headers for all requests
-axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
-
-// Optional: Add a request interceptor to include tokens
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem('accessToken'); // Retrieve your token from storage
-    if (accessToken) {
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default axiosInstance;
