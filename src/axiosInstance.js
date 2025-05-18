a91dafef5c74e94af0626a0b1579ab392b70c271
Sh-karan27 <shuklakaran107@gmail.com>
2025-05-18 17:01:51 +0530

    Refactor axios instance configuration and remove commented code

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,33 +1,24 @@
-// // src/axiosInstance.js
-// import axios from 'axios';
+import axios from 'axios';
 
-// const axiosInstance = axios.create({
-//   // baseURL: 'https://blog-api-ten-rust.vercel.app/api/v1',
-//   baseURL: 'http://localhost:8000/api/v1/',
+const axiosInstance = axios.create({
+  baseURL: 'https://blog-api-nxgh.onrender.com/api/v1',
 
-//   withCredentials: true, // Send cookies with requests
-// });
-
-// // Set default headers for all requests
-// axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
-
-// // Optional: Add a request interceptor to include tokens
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const accessToken = localStorage.getItem('accessToken'); // Retrieve your token from storage
-//     if (accessToken) {
-//       config.headers['Authorization'] = `Bearer ${accessToken}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: "https://blog-api-nxgh.onrender.com/api/v1",
-  withCredentials: true,
+  withCredentials: true, // Send cookies with requests
 });
+
+// Set default headers for all requests
+axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+
+// Optional: Add a request interceptor to include tokens
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken'); // Retrieve your token from storage
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
